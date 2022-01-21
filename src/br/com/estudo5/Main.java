c772eca5bf9d11037b6f7a6e0d2816985364550a
Guardianofligth <guardianofligth@outlook.com>
2022-01-21 03:30:26 -0300

    sobrecarga

---
+++ src/br/com/estudo5/Main.java
@@ -0,0 +1,13 @@
+package br.com.estudo5;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Quadrilatero
+        System.out.println("Exercício quandrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5, 5);
+        Quadrilatero.area(5, 6, 9);
+    }
+}
